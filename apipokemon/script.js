a45cc09ed66517b31c14e5994d5eb6d2561217dc
Racheth <91643079+Racheth@users.noreply.github.com>
2021-11-15 18:57:18 -0700

    Add files via upload

--- apipokemon/script.js
+++ apipokemon/script.js
@@ -1,10 +1,9 @@
-const pokeCard = document.querySelector("[datos-card]");
-const pokeName = document.querySelector("[datos-pokemon]");
-const pokeImg = document.querySelector("[datos-img]");
-const pokeContainer = document.querySelector("[datos-poke-img]");
-const pokeId = document.querySelector("[datos-id]");
-const pokeTypes = document.querySelector("[datos-types]");
-const pokeStats = document.querySelector("[datos-stats]");
+const Types = document.querySelector("[types]");
+const Name = document.querySelector("[pokemon]");
+const pokeStat = document.querySelector("[stats]");
+const Img = document.querySelector("[img]");
+const Id = document.querySelector("[id]");
+
 
 const buscador = event =>{
     event.preventDefault();
@@ -12,14 +11,40 @@ const buscador = event =>{
     fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
     .then(datos => datos.json())
     .then(response => Pokedatos(response))
+    
 } 
 
 const Pokedatos = datos =>{
+const imgpoke = datos.sprites.front_default;
 const{stats, types}= datos;
+Name.textContent= datos.name;
+Img.setAttribute(`src`, imgpoke);
+Id.textContent =`Num .${datos.id}`;
 console.log(datos)
-pokeName.textContent= datos.name;
-pokeImg.setAttribute("src",datos.sprites.front_default);
-pokeId.textContent =`Num .${datos.id}`;
+poketypess(types);
+pokestats(stats);
 }
 
+const poketypess = types =>{
+    Types.innerHTML='';
+types.forEach(type => {
+const typeElement =document.createElement("div");
+typeElement.textContent=type.type.name;
+Types.appendChild(typeElement);
+});
+}
+const pokestats = stats =>{
+    pokeStat.innerHTML='';
+    stats.forEach(stat => {
+const statsls =document.createElement("div");
+const statsl =document.createElement("div");
+const statsname =document.createElement("div");
+statsl.textContent= stat.stat.name;
+statsname.textContent= stat.base_stat;
+statsls.appendChild(statsl);
+statsls.appendChild(statsname);
+pokeStat.appendChild(statsls);
+}
 
+);
+}
